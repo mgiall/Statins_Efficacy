6f775088d66c13c46b6b6e1aa309eec22adf7465
averyyyang <averyyy@uw.edu>
2025-02-27 15:25:12 -0800

    Added package folder and test folder with unittest framework

---
+++ test/test_metadata_describe.py
@@ -0,0 +1,18 @@
+import unittest
+
+class TestDescribeDataframe(unittest.TestCase):
+
+    def test_smoke(self):
+        # TODO: Insert test code
+
+    def test_one_shot(self):
+        # TODO: Insert test code
+
+    def test_pattern(self):
+        # TODO: Insert test code
+
+    def test_edge(self):
+        # TODO: Insert test code
+
+if __name__ == '__main__':
+    unittest.main()
